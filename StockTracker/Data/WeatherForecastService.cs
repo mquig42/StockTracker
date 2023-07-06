9c1a47be3d076e52698b1933ab4ac6455d89b727
mquig42 <mquig42@users.noreply.github.com>
2023-07-05 23:55:57 -0400

    Add project files.

---
+++ StockTracker/Data/WeatherForecastService.cs
@@ -0,0 +1,22 @@
+namespace StockTracker.Data
+{
+    public class WeatherForecastService
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        public async Task<WeatherForecast[]> GetForecastAsync(DateOnly startDate)
+        {
+            //EDIT: Made this function async and added a delay to simulate waiting for a service to respond
+            await Task.Delay(2000);
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = startDate.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            }).ToArray();
+        }
+    }
+}
