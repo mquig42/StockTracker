571e666ef6698752816cc9f4c2f91dfd458dbaec
mquig42 <mquig42@users.noreply.github.com>
2023-07-07 22:10:50 -0400

    Added DbTrades class so I could use ReadAsync for the query

---
+++ StockTracker/Data/DbTrades.cs
@@ -0,0 +1,25 @@
+﻿namespace StockTracker.Data
+{
+    //Represents the Trades table
+    public class DbTrades
+    {
+        public DateOnly Date { get; set; }
+        public string? Symbol { get; set; }
+        public string? Type {  get; set; }
+        public string? Description { get; set; }
+        public decimal Qty { get; set; }
+        public decimal Cash { get; set; }
+        public decimal UnitsHeld { get; set; }
+
+        public DbTrades(DateOnly date, string? symbol, string? type, string? description, decimal qty, decimal cash, decimal unitsHeld)
+        {
+            Date = date;
+            Symbol = symbol;
+            Type = type;
+            Description = description;
+            Qty = qty;
+            Cash = cash;
+            UnitsHeld = unitsHeld;
+        }
+    }
+}
